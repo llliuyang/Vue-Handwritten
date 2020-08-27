335791f25135c8dee25cb066d8b84ad98dea99d6
llliuyang <1714610279@qq.com>
2020-08-27 15:38:16 +0800

    vuex的基本使用

---
+++ vuex/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    张三今年 {{ this.$store.state.age }} 岁啦！<br>
+    李四今年 {{ this.$store.getters.myAge }}  <br>
+    <button @click="$store.commit('changeAge',1)">同步更新+1</button>
+    <button @click="$store.dispatch('changeAge',1)">异步更新+1</button>
+
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+#app {
+  font-size: 22px;
+}
+</style>
---
+++ vuex/src/store/index.js
@@ -0,0 +1,29 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
+
+const store = new Vuex.Store({
+  state: {
+    age: 6
+  },
+  getters: {
+    myAge(state) {
+      return state.age + 20
+    }
+  },
+  mutations: {
+    changeAge(state, payload) {
+      state.age += payload
+    }
+  },
+  actions: {
+    changeAge({ commit }, payload) {
+      setTimeout(() => {
+        commit('changeAge', payload)
+      }, 1000)
+    }
+  }
+})
+
+export default store

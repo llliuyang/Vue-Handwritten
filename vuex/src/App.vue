0a0cbc705d47cd8524a62b184b466a389ddc674e
llliuyang <1714610279@qq.com>
2020-08-27 17:53:14 +0800

    vuex 基本版实现

--- vuex/src/App.vue
+++ vuex/src/App.vue
@@ -2,6 +2,7 @@
   <div id="app">
     张三今年 {{ this.$store.state.age }} 岁啦！<br>
     李四今年 {{ this.$store.getters.myAge }}  <br>
+    
     <button @click="$store.commit('changeAge',1)">同步更新+1</button>
     <button @click="$store.dispatch('changeAge',1)">异步更新+1</button>
 
--- vuex/src/vuex/store.js
+++ vuex/src/vuex/store.js
@@ -1,8 +1,59 @@
 import applyMixin from "./mixin";
+import { forEachValue } from "./util";
 
 export let Vue;
 export class Store {
+  constructor(options) {
+    // this.state = options.state //这样写数据不是响应式的，改变数据视图不会更新
+    const state = options.state
+    const computed = {}
 
+    this.getters = {}
+    forEachValue(options.getters, (fn, key) => {
+      computed[key] = () => {
+        return fn(this.state)
+      }
+      Object.defineProperty(this.getters, key, {
+        get: () => this._vm[key]
+      })
+    })
+
+    this._vm = new Vue({
+      data: {
+        $$state: state
+      },
+      computed
+    });
+
+    // Object.keys(options.getters).forEach(key => {
+    //   Object.defineProperty(this.getters, key, {
+    //     get: () => options.getters[key](this.state)
+    //   })
+    // })
+
+    this.mutations = {}
+    this.actions = {}
+    forEachValue(options.mutations, (fn, key) => {
+      this.mutations[key] = (payload) => fn(this.state, payload)
+    })
+
+    forEachValue(options.actions, (fn, key) => {
+      this.actions[key] = (payload) => fn(this, payload)
+    })
+
+  };
+
+  commit = (type, payload) => {
+    this.mutations[type](payload)
+  }
+
+  dispatch = (type, payload) => {
+    this.actions[type](payload)
+  }
+  
+  get state() {
+    return this._vm._data.$$state
+  };
 }
 // Vue.use()原理
 //  Vue.use = function(plugin) {
